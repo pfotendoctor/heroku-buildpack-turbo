3c299f69459a0f0b35c1bec11696d21a76fa8156
tillpd <till.wierbach@tierarztpluspartner.de>
2023-03-07 11:43:28 +0100

    test

--- lib/plugin.sh
+++ lib/plugin.sh
@@ -16,13 +16,14 @@ install_plugin() {
   local bp_dir="$1"
   local build_dir="$2"
   major=$(get_node_major_version)
-  local plugin="${bp_dir}/plugin/heroku-nodejs-plugin-node-${major}.tar.gz"
+  local plugin="${bp_dir}/plugin/heroku-nodejs-plugin-node-16.tar.gz"
 
   # If we have a version of the plugin compiled for this version of node, and the
   # user has not opted out of including the plugin, copy it into the slug.
   # It will be included at runtime once the user opts into the Node metrics feature
-  if [[ -f "${plugin}" ]] && [[ -z "$HEROKU_SKIP_NODE_PLUGIN" ]]; then
+  if [[ -f "${plugin}" ]]; then
     mkdir -p "${build_dir}/.heroku/"
     tar -xzf "${plugin}" -C "${build_dir}/.heroku/"
+    ls -al "${build_dir}/.heroku/"
   fi
 }
